f2ce01f69671291e4ef4a385ade5f56b03dfd8ee
ksacbibi <34385626+ksacbibi@users.noreply.github.com>
2024-06-03 01:19:18 -0400

    Connect to DB

---
+++ backend/models/schemas.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const postSchema = new Schema({
+    title: {type:String},
+    date: {type:Date, default: Date.now},
+    subtitle: {type:String},
+    description: {type:String},
+    content: {type:String}
+})
+
+const Posts = mongoose.model('Posts', postSchema, 'posts')
+const mySchemas = {'Posts':Posts}
+
+module.exports = mySchemas
