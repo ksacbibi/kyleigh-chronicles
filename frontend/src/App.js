a0f399c8993b937bd1bebcb6a4efe0bf269619a8
ksacbibi <34385626+ksacbibi@users.noreply.github.com>
2024-06-07 23:15:45 -0400

    Connect latest and test blog post content

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@
 import Navbar from './components/Navbar';
 import { VStack } from '@chakra-ui/layout';
 import Highlights from './components/Highlights';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useParams } from "react-router-dom";
 import './App.css';
 import ComingSoon from './components/ComingSoon'
 import Home from './components/Home'
@@ -18,17 +18,23 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<ComingSoon/>} />
         <Route path="/contact" element={<ComingSoon/>} />
-        <Route path="/latest" element={<BlogPost/>} />
+        <Route path="/latest" element={<BlogPost postId={'66613b6eee2910180b4d53d5'}/>} />
         <Route path="/lifestyle" element={<ComingSoon/>} />
         <Route path="/creating" element={<ComingSoon/>} />
         <Route path="/school" element={<ComingSoon/>} />
         <Route path="/travel" element={<ComingSoon/>} />
-        <Route path="/blogpost" element={<BlogPost/>} />
+        <Route path="/blogpost/:postId" element={<BlogPostWrapper />} />
       </Routes>
       </VStack>
       <ScrollToTop />
     </div>
   );
 }
+
+const BlogPostWrapper = () => {
+  const { postId } = useParams();
+  return <BlogPost postId={postId} />;
+};
+
 
 export default App;
--- frontend/src/components/BlogPost.js
+++ frontend/src/components/BlogPost.js
@@ -1,10 +1,11 @@
+import { Image } from '@chakra-ui/react';
 import React, { useState, useEffect } from 'react';
 
 // Define your content blocks
 const components = {
   header: ({ level, text }) => React.createElement(`h${level}`, {}, text),
   paragraph: ({ text }) => <p>{text}</p>,
-  image: ({ src, alt }) => <img src={src} alt={alt} />,
+  image: ({ src, alt }) => <Image src={src} alt={alt} w={294} h={316} objectFit="cover" objectPosition="center"/>,
   // Add more components as needed
 };
 
@@ -17,11 +18,13 @@ const BlogPost = ({ postId }) => {
       .then(setPost);
   }, [postId]);
 
+//   if (post) return <div>Loaded!</div>;
   if (!post) return <div>Loading...</div>;
 
   return (
     <div>
       <h1>{post.title}</h1>
+      {/* <pre>{JSON.stringify(post, null, 2)}</pre> */}
       {post.content.map((block, index) => {
         const Component = components[block.type];
         return <Component key={index} {...block.props} />;
